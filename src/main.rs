ce2fb6911952a662b63f21043a66a75f69c38363
faea726 <fantasy.eagle96@gmail.com>
2022-07-23 16:21:55 +0000

    modify main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod in_out_lib;
+use in_out_lib::read_data_from_file;
 
 fn main() {
     let filename = "data.txt";
-    let data = in_out_lib::read_data_from_file(filename, ":=");
+    let data = read_data_from_file(filename, ":=");
     for dt in data {
         print!("{:?}\n", dt);
     }
